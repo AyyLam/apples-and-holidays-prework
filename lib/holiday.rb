9f3df3d53e0b20d3d42fbc5aeb54013f5d36ecee
Anthony Lam <al3101@nyu.edu>
2018-05-07 17:42:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |holiday, items|
     items.each do |item|
       winter_supplies << item
-      endhttps://learn.co/tracks/web-development-fundamentals/intro-to-ruby/intro-to-hashes/manipulation-lab#
+      end
     end
   winter_supplies
 end
