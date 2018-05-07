af3f00a2246e0255220ee0b4a04afe6fa9d61a99
Anthony Lam <al3101@nyu.edu>
2018-05-07 16:55:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday| 
     puts "#{season}:".capitalize!
     holiday.each do |event, items| 
-      puts "#{event}: #{items}"
+      puts "#{event}: #{items}.join(" ")"
     end
   end
 end
